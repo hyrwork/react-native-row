a5f22fc18531a9b07be707a145ab20e9cc11c8ef
ssomnoremac <mosswd@gmail.com>
2017-01-11 17:03:03 -0500

    validate dial prop

--- View.js
+++ View.js
@@ -6,12 +6,13 @@ const RNView = RN.View;
 const View = (props) => {
 
     const { dial = 0, style, ...otherProps } = props;
+    const _dial = dial > 0 && dial < 10 ? dial : 0
 
-    const justifyContent = dial === 0 ? null : dial > 6 ? 'flex-end' :
-            dial > 3 ? 'center' : 'flex-start';
+    const justifyContent = _dial === 0 ? null : _dial > 6 ? 'flex-end' :
+            _dial > 3 ? 'center' : 'flex-start';
 
-    const alignItems = dial === 0 ? null : dial % 3 === 0 ? 'flex-end' :
-            dial % 3 === 2 ? 'center' : 'flex-start';
+    const alignItems = _dial === 0 ? null : _dial % 3 === 0 ? 'flex-end' :
+            _dial % 3 === 2 ? 'center' : 'flex-start';
 
     return (
         <RNView style={[{justifyContent, alignItems}, style]} {...otherProps} >
