33a79ac240c0328e2c99d789a15e09385c35636e
ssomnoremac <mosswd@gmail.com>
2017-11-21 14:34:25 -0500

    use class components, native-props, and caching

--- Row.js
+++ Row.js
@@ -1,40 +1,65 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import shorthandStyles from './shorthandStyles'
 
-const Row = (props) => {
+class Row extends React.Component {
+    
+    setNativeProps(nativeProps) {
+        this._root.setNativeProps(nativeProps)
+    }
 
-    const {
-        dial = 0,
-        flex: _flex,
-        spaceBetween,
-        spaceAround,
-        stretch,
-        margin,
-        padding,
-        style,
-        reverse,
-        ...otherProps,
-    } = props;
+    return() {
 
-    const _dial = dial > 0 && dial < 10 ? dial : 0;
-    const flex = typeof(_flex) === "number" ? _flex : !_flex ? null : 1
+        const {
+            dial = 0,
+            flex: _flex,
+            spaceBetween,
+            spaceAround,
+            stretch,
+            margin,
+            padding,
+            reverse,
+            style,
+            ...otherProps,
+        } = this.props;
 
-    const _shorthandStyles = shorthandStyles(margin, padding)
+        const _dial = dial > 0 && dial < 10 ? dial : 0;
+        const flex = typeof(_flex) === "number" ? _flex : !_flex ? null : 1
 
-    const justifyContent = spaceBetween ? 'space-between' : spaceAround ? 'space-around' : _dial === 0 ? null : _dial % 3 === 0 ? 'flex-end' :
-            _dial % 3 === 2 ? 'center' : 'flex-start';
+        const _shorthandStyles = shorthandStyles(margin, padding)
 
-    const alignItems = stretch ? 'stretch' : _dial === 0 ? null : _dial > 6 ? 'flex-end' :
-            _dial > 3 ? 'center' : 'flex-start';
+        const justifyContent = spaceBetween ? 'space-between' : spaceAround ? 'space-around' : _dial === 0 ? null : _dial % 3 === 0 ? 'flex-end' :
+                        _dial % 3 === 2 ? 'center' : 'flex-start';
 
-    const flexDirection = reverse ? 'row-reverse' : 'row';
+        const alignItems = stretch ? 'stretch' : _dial === 0 ? null : _dial > 6 ? 'flex-end' :
+                    _dial > 3 ? 'center' : 'flex-start';
 
-    return (
-        <View style={[{flexDirection, justifyContent, alignItems, flex}, _shorthandStyles, style]} {...otherProps} >
-            {props.children}
-        </View>
-    );
-};
+        const flexDirection = reverse ? 'row-reverse' : 'row';
+
+        // memoize the cached styles
+        const fixedStyles = {dial, _flex, spaceBetween, spaceAround, stretch, margin, padding, reverse}
+        // const index = JSON.stringify(fixedStyles)
+        // let cachedStyleNumber = null
+        // if (index in cachedStyles) {
+        //     cachedStyleNumber = cachedStyles[index]
+        // }
+        // else {
+        const toBeCachedStyle = {
+            flexDirection,
+            justifyContent,
+            alignItems,
+            flex,
+            ...fixedStyles,
+            ..._shorthandStyles,
+        }
+        cachedStyleNumber = StyleSheet.create(toBeCachedStyle)
+
+        return (
+            <View style={[cachedStyleNumber, style]} {...otherProps} >
+                {this.props.children}
+            </View>
+        );
+    };
+}
 
 export default Row
--- View.js
+++ View.js
@@ -3,39 +3,58 @@ import RN from 'react-native';
 import shorthandStyles from './shorthandStyles'
 const RNView = RN.View;
 
-const View = (props) => {
+class View extends React.Component {
 
-    const {
-        dial = 0,
-        flex: _flex,
-        style,
-        spaceBetween,
-        spaceAround,
-        stretch,
-        margin,
-        padding,
-        reverse,
-        ...otherProps,
-    } = props;
+    setNativeProps(nativeProps) {
+        this._root.setNativeProps(nativeProps)
+    }
 
-    const _dial = dial > 0 && dial < 10 ? dial : 0;
-    const flex = typeof(_flex) === "number" ? _flex : !_flex ? null : 1
+    render() {
 
-    const _shorthandStyles = shorthandStyles(margin, padding)
+        const {
+            dial = 0,
+            flex: _flex,
+            style,
+            spaceBetween,
+            spaceAround,
+            stretch,
+            margin,
+            padding,
+            reverse,
+            ...otherProps,
+        } = this.props;
 
-    const justifyContent = spaceBetween ? 'space-between' : spaceAround ? 'space-around' : _dial === 0 ? null : _dial > 6 ? 'flex-end' :
-            _dial > 3 ? 'center' : 'flex-start';
+        const _dial = dial > 0 && dial < 10 ? dial : 0;
+        const flex = typeof(_flex) === "number" ? _flex : !_flex ? null : 1
 
-    const alignItems = stretch ? 'stretch' : _dial === 0 ? null : _dial % 3 === 0 ? 'flex-end' :
-            _dial % 3 === 2 ? 'center' : 'flex-start';
+        const _shorthandStyles = shorthandStyles(margin, padding)
 
-    const flexDirection = reverse ? 'column-reverse' : 'column';
+        const justifyContent = spaceBetween ? 'space-between' : spaceAround ? 'space-around' : _dial === 0 ? null : _dial > 6 ? 'flex-end' :
+                _dial > 3 ? 'center' : 'flex-start';
 
-    return (
-        <RNView style={[{flexDirection, justifyContent, alignItems, flex}, _shorthandStyles, style]} {...otherProps} >
-            {props.children}
-        </RNView>
-    );
+        const alignItems = stretch ? 'stretch' : _dial === 0 ? null : _dial % 3 === 0 ? 'flex-end' :
+                _dial % 3 === 2 ? 'center' : 'flex-start';
+
+        const flexDirection = reverse ? 'column-reverse' : 'column';
+
+        const fixedStyles = {dial, _flex, spaceBetween, spaceAround, stretch, margin, padding, reverse}    
+
+        const toBeCachedStyle = {
+            flexDirection,
+            justifyContent,
+            alignItems,
+            flex,
+            ...fixedStyles,
+            ..._shorthandStyles,
+        }
+        cachedStyleNumber = StyleSheet.create(toBeCachedStyle)
+
+        return (
+            <RNView style={[cachedStyleNumber, style]} {...otherProps} >
+                {this.props.children}
+            </RNView>
+        );
+    }
 };
 
 export default View
